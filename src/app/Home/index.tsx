5b4ace50c6f3ec8a8c76f32b35e5e84850b4784a
NatanPaulo <105021073+NatanPaulo@users.noreply.github.com>
2025-07-13 00:29:12 -0300

    Componentes - Componente Filter

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,20 +1,25 @@
 // Executar local: npx expo start
 // Executar externo: npx expo start --tunnel
 
-import { View, Image } from "react-native";
+import { View, Image, ImageBackgroundComponent } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
       <View style={styles.form}>
-        <Input placeholder="Digite seu e-mail" />
+        <Input placeholder="O que você deseja comprar?" />
         <Button title="Entrar" />
       </View>
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive={true} />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
