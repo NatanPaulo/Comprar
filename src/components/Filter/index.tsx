835d3a44145042a90e6998dcbd5d31ba22724e45
NatanPaulo <105021073+NatanPaulo@users.noreply.github.com>
2025-07-14 23:27:40 -0300

    Componentes - Ícones

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,10 +1,13 @@
 import { TouchableOpacity, Text, TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
 import { FilterStatus } from "@/types/FilterStatus";
+//OPCIONAL: import { AntDesign, MaterialIcons } from "@expo/vector-icons"; //https://icons.expo.fyi/Index
+//Instalar nova biblioteca de ícones: npx expo install react-native-svg lucide-react-native
+import { CircleCheck } from "lucide-react-native"; // https://lucide.dev/
 
 type Props = TouchableOpacityProps & {
-  status: FilterStatus;
-  isActive: boolean;
+  status: FilterStatus; // Status do filtro (pendente ou comprado)
+  isActive: boolean; // Indica se o filtro está ativo
 };
 
 export function Filter({ status, isActive, ...rest }: Props) {
@@ -14,6 +17,9 @@ export function Filter({ status, isActive, ...rest }: Props) {
       activeOpacity={0.8}
       {...rest}
     >
+      {/* <AntDesign name="home" size={18} />
+      <MaterialIcons name="shopping-cart" size={18} /> */}
+      <CircleCheck size={18} color={isActive ? "#4CAF50" : "#9E9E9E"} />
       <Text style={styles.title}>
         {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
       </Text>
--- src/types/FilterStatus.ts
+++ src/types/FilterStatus.ts
@@ -1,4 +1,4 @@
 export enum FilterStatus {
-  PENDING = "pending",
-  DONE = "done",
+  PENDING = "pending", // Status para pendentes
+  DONE = "done", // Status para comprados
 }
